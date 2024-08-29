b0e47db2f3e9e2a3dbf8c0977e589123464878db
2023aa05358@wilp.bits-pilani.ac.in <2023aa05358@wilp.bits-pilani.ac.in>
2024-08-29 05:52:23 +0000

    code of Find whether the number is prime or not

---
+++ Find_Prime_Number.py
@@ -0,0 +1,15 @@
+# Databricks notebook source
+def is_prime(number):
+    if number <= 1:
+        return False
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+# Example usage
+number = 29
+if is_prime(number):
+    print(f"{number} is a prime number")
+else:
+    print(f"{number} is not a prime number")
